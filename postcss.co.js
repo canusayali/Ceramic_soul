d3e416b830479c33196995328fa4f5a1eb134f0e
ali <mansurovumid843@gmail.com>
2026-04-15 22:46:26 +0500

    first commit

---
+++ postcss.co.js
@@ -0,0 +1,16 @@
+// const { plugin } = require("postcss");
+// import pxtorem from 'postcss-pxtorem'
+
+// export default {
+//     plugins: {
+//         "postcss-pxotrem": {
+//             rootValue: 16,
+//             propList: ["*"],
+//             selectorBlackList: [],
+//             replace: true,
+//             mediaQuery: true,
+//             minPixelValue: 0,
+//         },
+
+//     },
+// };
